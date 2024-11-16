d5023b6cb76df34a3d249397cb059af61ed41047
Jhordy272 <jaagatonp@udistrital.edu.co>
2024-11-15 23:34:08 -0500

    Add new routes for CheckBoards and CheckBoardsAssignments

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -3,7 +3,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Login from "../pages/Login";
 import Projects from "../pages/Projects"; // Importa el nuevo componente
-import Management from "../pages/Management"; // Importa el nuevo componente
+import CheckBoards from "../pages/CheckBoards";
+import CheckBoardAssignments from "../pages/CheckBoardAssignments";
 
 function AppRoutes() {
   return (
@@ -12,7 +13,9 @@ function AppRoutes() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/projects" element={<Projects />} /> {/* Nueva ruta para proyectos */}
-        <Route path="/management" element={<Management />} /> {/* Nueva ruta para gestión */}
+        <Route path="/checkboards" element={<CheckBoards />} /> {/* Nueva ruta para checkboards */}
+        <Route path="/checkboardsAssignment" element={<CheckBoardAssignments />} /> {/* Nueva ruta para asignaciones de checkboards */}
+        <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </Router>
   );
