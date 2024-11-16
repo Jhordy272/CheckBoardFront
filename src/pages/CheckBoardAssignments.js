f33e0018ae9ad255c6609712daa38497ea5c5eff
Jhordy272 <jaagatonp@udistrital.edu.co>
2024-11-15 23:34:35 -0500

    Create page CheckBoardAssignment

---
+++ src/pages/CheckBoardAssignments.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CheckBoardAssignments() {
+  return (
+    <div>
+      <h1>Asignaciones de tableros de control</h1>
+      <p>Este es un nuevo componente</p>
+    </div>
+  );
+}
+
+export default CheckBoardAssignments;
